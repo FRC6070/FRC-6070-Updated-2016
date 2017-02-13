fa163d4a688c64fc76e12f139ee66e1bca867de2
LanchGear <guostephen327@gmail.com>
2017-02-13 11:04:30 -0500

    Add files via upload

--- src/org/usfirst/frc/team6070/robot/subsystems/Chassis.java
+++ src/org/usfirst/frc/team6070/robot/subsystems/Chassis.java
@@ -12,39 +12,35 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Chassis extends Subsystem {
 	
-	Victor BL = new Victor (RobotMap.backLeft);
-	Victor BR = new Victor (RobotMap.backRight);
-	Victor FL = new Victor (RobotMap.frontLeft);
-	Victor FR = new Victor (RobotMap.frontright);
+	Victor BL = new Victor (RobotMap.BackLeft);
+	Victor BR = new Victor (RobotMap.BackRight);
+	Victor FL = new Victor (RobotMap.FrontLeft);
+	Victor FR = new Victor (RobotMap.FrontRight);
 	
-	RobotDrive drive = new RobotDrive (BL, BR, FL, FR);
-	
-	
+	RobotDrive drive = new RobotDrive(BL, BR, FL, FR);
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
     public void initDefaultCommand() {
-    	drive.setInvertedMotor(MotorType.kFrontLeft, true);
+    	drive.setInvertedMotor(MotorType.kFrontRight, true);
     	drive.setInvertedMotor(MotorType.kRearRight, true);
-
-    		
-    	}
-    public void drive(double y, double z){
-    	drive.arcadeDrive(y, z);
+    	
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void drive(double y, double x){
+    	drive.arcadeDrive(y, x);
     	
     }
     
     public void stop (){
     	drive.arcadeDrive(0, 0);
-    	
     }
     
-    public void spin (){
+    public void spin(){
     	drive.arcadeDrive(0, 1);
     }
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
-
+}
 
